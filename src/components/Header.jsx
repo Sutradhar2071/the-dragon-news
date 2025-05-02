8b84730a0acaceb6992b362575b607fcc9e1de2b
Sutradhar2071 <sutradharringku@gmail.com>
2025-05-02 18:18:38 +0600

    font add & date

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from '../assets/logo.png'
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
         <div className='flex justify-center items-center flex-col gap-3 p-4'>
             <img className='w-[400px]' src={logo} alt="" />
-            <p>Journalism Without Fear or Favour</p>
+            <p className='text-accent'>Journalism Without Fear or Favour</p>
+            <p className='font-semibold text-accent'>{format(new Date(), "EEEE, MMMM dd, yyyy")}</p>
         </div>
     );
 };
