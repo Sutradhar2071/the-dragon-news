06787352d98fd6e19efb5fbdb172ed399a75ff54
Sutradhar2071 <sutradharringku@gmail.com>
2025-05-03 17:42:16 +0600

    data load and display show

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import { el } from 'date-fns/locale';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
-    const {id} = useParams();
-    console.log(id)
+    const [categoryNews, setCategoryNews] = useState([]);
+    const { id } = useParams();
+    const data = useLoaderData();
+    useEffect(() => {
+        if (id == '0') {
+            setCategoryNews(data);
+            return;
+        }
+        else if (id == '1') {
+            const filterNews = data.filter((news) => news.others.is_today_pick == true);
+            setCategoryNews(filterNews);
+        }
+        else {
+            const filterNews = data.filter((news) => news.category_id == id);
+            setCategoryNews(filterNews)
+        }
+
+    }, [data, id])
     return (
         <div>
-            category{id}
+            category{categoryNews.length}
         </div>
     );
 };
